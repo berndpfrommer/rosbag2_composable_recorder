3628520e73d5de42c215863d2484b2cf4861af79
Bernd Pfrommer <bernd.pfrommer@gmail.com>
2024-05-31 20:28:27 -0400

    enable build on rosbag2_tansport >= v0.25 (#3)
    * enable build on rosbag2_transport >= v0.25 to build on rolling and iron

--- src/composable_recorder.cpp
+++ src/composable_recorder.cpp
@@ -45,7 +45,11 @@ ComposableRecorder::ComposableRecorder(const rclcpp::NodeOptions & options)
     RCLCPP_INFO_STREAM(get_logger(), "recording topic: " << topic);
   }
   // set storage options
+#ifdef USE_GET_STORAGE_OPTIONS
+  rosbag2_storage::StorageOptions & sopt = get_storage_options();
+#else
   rosbag2_storage::StorageOptions & sopt = storage_options_;
+#endif
   sopt.storage_id = declare_parameter<std::string>("storage_id", "sqlite3");
   sopt.max_cache_size = declare_parameter<int>("max_cache_size", 100 * 1024 * 1024);
   const std::string bag_name = declare_parameter<std::string>("bag_name", "");
@@ -56,14 +60,29 @@ ComposableRecorder::ComposableRecorder(const rclcpp::NodeOptions & options)
   }
 
   // set recorder options
+#ifdef USE_GET_RECORD_OPTIONS
+  rosbag2_transport::RecordOptions & ropt = get_record_options();
+#else
   rosbag2_transport::RecordOptions & ropt = record_options_;
+#endif
+#ifdef USE_ALL_TOPICS
+  ropt.all_topics = declare_parameter<bool>("record_all", false);
+#else
   ropt.all = declare_parameter<bool>("record_all", false);
+#endif
   ropt.is_discovery_disabled = declare_parameter<bool>("disable_discovery", false);
   ropt.rmw_serialization_format = declare_parameter<std::string>("serialization_format", "cdr");
   ropt.topic_polling_interval = std::chrono::milliseconds(100);
   ropt.topics.insert(ropt.topics.end(), topics.begin(), topics.end());
 
-  stop_discovery_ = ropt.is_discovery_disabled;
+  if (ropt.is_discovery_disabled) {
+#ifdef USE_STOP_DISCOVERY
+    stop_discovery();
+#else
+    stop_discovery_ = ropt.is_discovery_disabled;
+#endif
+  }
+
   if (declare_parameter<bool>("start_recording_immediately", false)) {
     record();
   } else {
